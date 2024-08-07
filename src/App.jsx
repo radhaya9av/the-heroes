e9c217ba406f3d7b1671f328ac39eb813ab846b4
radhaya9av <radhaya9av@gmail.com>
2024-08-07 12:31:08 +0530

    navbarComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1>The Heroes</h1>
+    <div className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
+      <Navbar />
+    </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { FaGithub } from "react-icons/fa6";
+
+function Navbar() {
+
+  return (
+    <>
+      <div className="w-full py-4 flex items-center justify-between">
+        <div className="right text-black-100 font-bold">
+            <a href="/">
+            <h1>The Heroes</h1>
+            </a>
+        </div>
+        <div className="left">
+          <div className="btn-group flex items-center gap-2">
+            <Button
+              className="source-code-btn flex items-center gap-2"
+              onClick={() => {
+                window.location.href = "https://github.com/radhaya9av/the-heroes";
+              }}
+            >
+                <FaGithub /> Star
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
