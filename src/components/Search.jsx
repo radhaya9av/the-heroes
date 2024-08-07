e313410cfe7c01a836561daba75c689b1d71b57c
radhaya9av <radhaya9av@gmail.com>
2024-08-07 21:38:07 +0530

    searchComponentFunctioning

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import md5 from "md5";
 import Characters from "./Characters";
 import Comics from "./Comics";
 
@@ -7,10 +8,56 @@ function Search() {
   const [characterData, setCharacterData] = useState(null);
   const [comicData, setComicData] = useState(null);
 
+  const privateKey = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
+  const publicKey = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
     getCharacterData();
+  };
+
+  const getCharacterData = () => {
+    setCharacterData(null);
+    setComicData(null);
+
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
+
+    const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&nameStartsWith=${characterName}&limit=100`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setCharacterData(result.data);
+        console.log(result.data);
+      })
+      .catch(() => {
+        console.log("error while getting character data");
+      });
+  };
+
+  const getComicData = (characterId) => {
+    window.scrollTo({ top: 0, left: 0 });
+
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
+
+    const url = `https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setComicData(result.data);
+        console.log(result.data);
+      })
+      .catch(() => {
+        console.log("error while getting comic data");
+      });
+  };
+
+  const generateHash = (timeStamp) => {
+    return md5(timeStamp + privateKey + publicKey);
   };
 
   const handleChange = (event) => {
