4974d3adb39f157292e34535ac280cbb5e4286b8
radhaya9av <radhaya9av@gmail.com>
2024-08-07 21:22:54 +0530

    comicsComponent

---
+++ src/components/Comics.jsx
@@ -0,0 +1,31 @@
+function Comics({ data, onClick }) {
+  return (
+    <div className="comics">
+      {data.map((dataItem) => {
+        const detailsUrl = dataItem.urls.find(
+          (element) => element["type"] === "detail"
+        ).url;
+
+        return (
+          <a
+            key={dataItem.id}
+            className="comicCard"
+            style={{
+              background: `url(${dataItem.thumbnail.path}.${dataItem.thumbnail.extension}) no-repeat center`,
+              backgroundSize: "cover",
+            }}
+            onClick={() => onClick(dataItem.id)}
+            href={detailsUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="caption">{dataItem.title}</div>
+            <div className="caption bottom">View Comic Details</div>
+          </a>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Comics;
